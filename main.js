ed2e778e2af4e6cd67c04f53a2280d3e38ed5428
Dylan <cs117x@users.noreply.github.com>
2019-03-20 18:18:24 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 
 function search() {
-	
+	var free_rooms = []
 	var week = document.getElementById("week").value;
 	var day = document.getElementById("day").value;
 	var period = document.getElementById("period").value;
+	var container = document.getElementById("free-rooms");
+
 
 	var query = week + day + period
 
 	for (var i = 0; i < data.length; i++) {
 		if (data[i].FIELD1 == query) {
 			for (x in data[i]) {
-				console.log(x)
+				free_rooms.push(x)
+				number = data[i]
 			}
 		}	
 	}
+	for (room_location in free_rooms) {
+		var t = document.createElement('li');
+		t.textContent = data[room_location];
+		container.appendChild(t);
+	}
+	
 }
