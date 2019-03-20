dbbc1850529be8577dd445dc965a87666640b5f2
Dylan <cs117x@users.noreply.github.com>
2019-03-20 14:38:35 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function search() {
+	
+	var week = document.getElementById("week").value,
+	var day = document.getElementById("day").value,
+	var period = document.getElementById("period").value;
+
+	var query = week + day + period
+
+	console.log(query)
+
+}
